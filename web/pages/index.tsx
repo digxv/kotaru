8018d5790d26273c59a78b1306357a5d78e6a011
hemrathore <digvijaysra@gmail.com>
2021-11-03 23:57:54 +0530

    homepage idk

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,9 +1,10 @@
 import React, { useRef } from "react";
+import AppLayout from "../components/AppLayout";
 
 export default function Home() {
   return (
-    <>
+    <AppLayout pageTitle="Kotaru.xyz">
 
-    </>
+    </AppLayout>
   )
 }
