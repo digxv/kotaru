ebab905c789111d4de7d075d172fe49aeb912d03
hemrathore <digvijaysra@gmail.com>
2021-11-04 00:54:48 +0530

    buy product func (not tested)

--- contracts/Kotaru.sol
+++ contracts/Kotaru.sol
@@ -49,4 +49,14 @@ contract Kotaru {
 
         emit ObjectPublished(objectCount, _name, _ipfs_hash, _price, publisher);
     }
+
+    function buyObject(uint256 _id) public payable {
+        require(_id > 0 && _id <= objectCount);
+        Object memory _object = objects[_id];
+        require(msg.value = _object.price);
+        address payable _publisher = _object.publisher;
+        address(_publisher).transfer(msg.value);
+        _object.downloads = _object.downloads + 1;
+        objects[_id] = _object;   
+    }
 }
